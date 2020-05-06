7c6e91da164c71c95cad0140f0fcb4385e27c487
Ariful Islam <ariful.islamny105@gmail.com>
2020-05-06 04:43:50 -0400

    Update info for amazon

---
+++ Amazon/src/main/java/AmazonHomePage.java
@@ -0,0 +1,27 @@
+package main.java;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class AmazonHomePage {
+
+    public WebDriver driver = null;
+
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Spare Computer\\Practice\\Driver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.navigate().to("https://www.amazon.com");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void cleanUp(){
+        driver.close();
+    }
+}
